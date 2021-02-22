0db9cfc0ba9e5788c23f5774ebedab100f576b81
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2021-02-22 17:52:59 +0200

    Added bricks

---
+++ Assets/Scripts/Behaviours/BrickBehaviour.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBehaviour : MonoBehaviour
+{
+
+    public int hits = 2;
+    public int points = 100;
+
+
+    SpriteRenderer spriteRenderer;
+    Color white = new Color(255, 255, 255);
+    Color originalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hits--;
+        //Score points
+        if (hits <= 0)
+        {
+            Destroy(gameObject);
+        };
+
+        spriteRenderer.color = white;
+        Invoke(nameof(RestoreOriginalColor), 0.1f);
+    }
+
+    void RestoreOriginalColor()
+    {
+        spriteRenderer.color = originalColor;
+    }
+}
