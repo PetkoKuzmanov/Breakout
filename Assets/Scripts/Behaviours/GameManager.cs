5ca987c80a38d5b7db8b25178da619cfe937448f
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2021-02-23 16:00:48 +0200

    Fixed a bug with the Event System

--- Assets/Scripts/Behaviours/GameManager.cs
+++ Assets/Scripts/Behaviours/GameManager.cs
@@ -1,21 +1,132 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject ballPrefab;
     public GameObject platformPrefab;
+    public TextMeshProUGUI textScore;
+    public TextMeshProUGUI textLives;
+    public TextMeshProUGUI textLevel;
+
+    public GameObject panelMenu;
+    public GameObject panelPlay;
+    public GameObject panelLevelCompleted;
+    public GameObject panelGameOver;
+
+    public static GameManager Instance { get; private set; }
+
+    public enum State { MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER}
+    State state;
+
+    private int score;
+    public int Score
+    {
+        get { return score; }
+        set { score = value;
+            textScore.text = "Score: " + score;
+        }
+    }
+
+    private int level;
+    public int Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    private int lives;
+    public int Lives
+    {
+        get { return lives; }
+        set { lives = value; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
+        ChangeState(State.MENU);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
+    public void ChangeState(State newState)
+    {
+        EndState();
+        BeginState(newState);
+    }
+
+    void BeginState(State newState)
+    {
+        switch (newState)
+        {
+            case State.MENU:
+                panelMenu.SetActive(true);
+                break;
+            case State.INIT:
+                panelPlay.SetActive(true);
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                panelLevelCompleted.SetActive(true);
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                panelGameOver.SetActive(true);
+                break;
+        }
+    }
+
+    void EndState()
+    {
+        switch (state)
+        {
+            case State.MENU:
+                panelMenu.SetActive(false);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                panelLevelCompleted.SetActive(false);
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                panelPlay.SetActive(false);
+                panelGameOver.SetActive(false);
+                break;
+        }
+    }
+
+    public void StartGameClicked()
+    {
+        Debug.Log("ASd");
+        ChangeState(State.INIT);
     }
 }
